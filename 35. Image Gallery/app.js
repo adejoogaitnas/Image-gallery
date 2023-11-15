c8f352c6713207d42861b35697c4bd7c815de6ea
adejoogaitnas <adejoogaitnas@gmail.com>
2023-11-15 12:12:36 +0100

    Primer commit-hotfixes-implementación de un if

--- 35. Image Gallery/app.js
+++ 35. Image Gallery/app.js
@@ -22,20 +22,28 @@
 
 // frutas3.forEach(recorrerArray);
 
-var images = document.querySelectorAll("img");
-var wrapper = document.getElementById("wrapper");
-var imgWrapper = document.getElementById("fullImg");
-var close = document.querySelector("span");
+var images = document.querySelectorAll("img");//Seleccionamos todos los elementos img
+var wrapper = document.getElementById("wrapper");//Seleccionamos el contenedor de la imagen
+var imgWrapper = document.getElementById("fullImg");//Seleccionamos la imagen que va a esta dentro del contenedor
+var close = document.querySelector("span");//Seleccionamos la etiqueta span
 
-images.forEach((img, index) => {
-    img.addEventListener("click", () => {
-        openModal(`images/img${index}.jpg`);
-    });
+images.forEach((img, index) => {//Images es el elemento iterable ejecutamos un forEach para ejecutar la funcion fecha sobre cada elemento iterable
+    if (index > 0) {//Si el indice es mayor a 0, ejecutamos la funcion flecha de lo contrario no
+        img.addEventListener("click", () => {//img elemento al que se está adjuntando el evento
+                                            //click tipo de evento
+                                            //funcion flecha es lo que va hacer el click
+            openModal(`images/img${index}.jpg`);
+        });
+    }
 });
 
 function openModal(recoger) {
-    wrapper.style.display = "flex";
-    imgWrapper.src = recoger;
-    imgWrapper.alt = "imagen dinámica"; 
+    wrapper.style.display = "flex";//Cambiamos el display del contenedor por flex
+    imgWrapper.src = recoger;//Seleccionamos la dirección de la imagen 
+    imgWrapper.alt = "imagen dinámica";//Añadimos información a las imagenes
 }
-close.addEventListener("click", () =>wrapper.style.display="none")
+close.addEventListener("click", () => wrapper.style.display = "none");//Cuando le damos a la X cambiamos el display a none cerrandose la ventana, y mostrandonos la galeria entera.
+
+
+
+
